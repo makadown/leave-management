cb7225afb4a995de012c8e785bb4b68b7be33698
makadown <maseflores79@gmail.com>
2020-07-22 21:02:46 -0500

    Configurando View Models y AutoMapper

---
+++ leave-management/Models/LeaveTypeViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Models
+{
+   /* public class LeaveTypeViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public DateTime DateCreated { get; set; }
+    }*/
+
+    public class DetailsLeaveTypeViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+
+    public class CreateLeaveTypeViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
