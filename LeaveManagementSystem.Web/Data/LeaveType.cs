9e444ddf82a0a28f71ea5ee3e1551a4a2d7328ca
Mario Arturo Serrano Flores <mario.serrano@imssbienestar.gob.mx>
2024-11-06 11:16:02 -0800

    Creado CRUD para tipo de licencia

--- LeaveManagementSystem.Web/Data/LeaveType.cs
+++ LeaveManagementSystem.Web/Data/LeaveType.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace LeaveManagementSystem.Web.Data;
@@ -7,6 +8,8 @@ public class LeaveType
 {
     public int Id { get; set; }
     [Column(TypeName = "nvarchar(150)")]
+    [Display(Name = "Nombre")]
     public string Name { get; set; } = string.Empty;
+    [Display(Name = "Número de dias")]
     public int NumberOfDays { get; set; }
 }
